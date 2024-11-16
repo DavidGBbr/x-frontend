cdd3f47be6d77be7d9b516fd2093e4c6fc3656a0
davidGBbr <davidgomesbrigido@gmail.com>
2024-11-16 12:21:46 -0300

    feat: componente logo

---
+++ src/app/(auth)/signin/page.tsx
@@ -0,0 +1,12 @@
+import Logo from "@/components/ui/logo";
+import React from "react";
+
+const SignIn = () => {
+  return (
+    <div className="max-w-lg mx-auto mt-12 px-6">
+      <Logo size={56} />
+    </div>
+  );
+};
+
+export default SignIn;
