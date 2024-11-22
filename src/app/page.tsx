f5fcda4af7f861b5b55723e483e8c351b95f1eec
davidGBbr <davidgomesbrigido@gmail.com>
2024-11-22 08:30:47 -0300

    feat: redireciona para home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import Logo from "@/components/ui/logo";
 import React from "react";
 
 const Page = () => {
-  return <div>...</div>;
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <Logo size={80} />
+    </div>
+  );
 };
 
 export default Page;
