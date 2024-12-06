e2e2ff4dbc276bf3022b8f614019e08f55ae4efa
davidGBbr <davidgomesbrigido@gmail.com>
2024-12-06 00:51:50 -0300

    feat: Criar header home

--- src/app/(ui)/home/page.tsx
+++ src/app/(ui)/home/page.tsx
@@ -1,7 +1,12 @@
+import HomeHeader from "@/components/home/home-header";
 import React from "react";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+      <HomeHeader />
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/components/home/home-menu.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type Props = {
+  closeAction: () => void;
+};
+
+const HomeMenu = ({ closeAction }: Props) => {
+  return <div>...</div>;
+};
+
+export default HomeMenu;
