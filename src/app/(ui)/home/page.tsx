fb371fb638faac64bdfb19f8ba47112fab0b76af
davidGBbr <davidgomesbrigido@gmail.com>
2024-12-08 10:27:43 -0300

    feat: Criar componente de postar

--- src/app/(ui)/home/page.tsx
+++ src/app/(ui)/home/page.tsx
@@ -1,10 +1,12 @@
 import HomeHeader from "@/components/home/home-header";
+import TweetPost from "@/components/tweet/tweet-post";
 import React from "react";
 
 const Home = () => {
   return (
     <div>
       <HomeHeader />
+      <TweetPost />
     </div>
   );
 };
