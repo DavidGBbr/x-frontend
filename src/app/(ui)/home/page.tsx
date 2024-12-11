b2d7834d8434c980a544eaa17279418bdabf6bf3
davidGBbr <davidgomesbrigido@gmail.com>
2024-12-11 08:56:13 -0300

    feat: criar feed tweet

--- src/app/(ui)/home/page.tsx
+++ src/app/(ui)/home/page.tsx
@@ -1,12 +1,14 @@
 import HomeHeader from "@/components/home/home-header";
 import TweetPost from "@/components/tweet/tweet-post";
 import React from "react";
+import HomeFeed from "../../../components/home/home-feed";
 
 const Home = () => {
   return (
     <div>
       <HomeHeader />
       <TweetPost />
+      <HomeFeed />
     </div>
   );
 };
