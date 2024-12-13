ac8f187a600dc966bc6bd68743a3adb405d71e5c
davidGBbr <davidgomesbrigido@gmail.com>
2024-12-13 07:56:20 -0300

    feat: criar funcao de tempo

--- src/components/tweet/tweet-item.tsx
+++ src/components/tweet/tweet-item.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { Tweet } from "@/types/tweet";
+import { formatRelativeTime } from "@/utils/format-relative";
 import { faComment, faHeart } from "@fortawesome/free-regular-svg-icons";
 import {
   faRetweet,
@@ -37,7 +38,9 @@ const TweetItem = ({ tweet }: Props) => {
             <Link href={`/${tweet.user.slug}`} />
             {tweet.user.name}
           </div>
-          <div className="text-xs text-gray-500">@{tweet.user.slug}</div>
+          <div className="text-xs text-gray-500">
+            @{tweet.user.slug} - {formatRelativeTime(tweet.dataPost)}
+          </div>
         </div>
         <div className="py-4 text-lg">{tweet.body}</div>
         {tweet.image && (
